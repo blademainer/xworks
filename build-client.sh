82bb9eeac997309f96e44c904cdd0de33deef072
blademainer <blademainer@gmail.com>
2018-06-05 00:39:03 +0800

    xy tag

--- build-client.sh
+++ build-client.sh
@@ -4,8 +4,7 @@ SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 ${SHELL_FOLDER}/build.sh "$arch"
 version="v`date  +"%Y%m%d%H%M%s"`"
 REPO=blademainer/xworks:client-${arch}-${version}
-XY_TAG=${XY_REPO:-hub.xycloud.com/18504}:client-${arch}-${version}
-echo "repo: $REPO"
+XY_TAG=${XY_REPO:-hub.xycloud.com/18504}/xworks:client-${arch}-${version}
 docker build -f dockerfile-client -t $REPO -t ${XY_TAG} .
 docker push $REPO
 docker push ${XY_TAG}
--- build-server.sh
+++ build-server.sh
@@ -4,7 +4,7 @@ SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 ${SHELL_FOLDER}/build.sh "$arch"
 version="v`date  +"%Y%m%d%H%M%s"`"
 REPO=blademainer/xworks:server-${arch}-${version}
-XY_TAG=${XY_REPO:-hub.xycloud.com/18504}:server-${arch}-${version}
+XY_TAG=${XY_REPO:-hub.xycloud.com/18504}/xworks:server-${arch}-${version}
 docker build -f dockerfile-server -t $REPO -t ${XY_TAG} .
 docker push $REPO
 docker push ${XY_TAG}
